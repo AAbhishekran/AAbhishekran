ee96bfc78d2b2e5edc5f8bd1667d8eb1690f91af
Mind game <abhirana2865@gmail.com>
2024-02-07 23:41:10 +0530

    i created the edit funtion in the gui aap

--- main.py
+++ main.py
@@ -5,19 +5,25 @@ import function
 list_box = ss.Listbox(values = function.file(), key="main",size=[45,15])
 Layout = [[ss.Text("This is my todoaap",key="text")],
           [ss.InputText(tooltip="Enter your todo id her",key="todo"),
-           ss.Button("Add")],[list_box]
+           ss.Button("Add")],[list_box,ss.Button("edit",key="edit")]
           ]
 window = ss.Window(title="Todosaap" , layout=Layout,font=("Bold",10))
 
 while True:
     event , values = window.read()
-    print(event)
-    print(values)
     match event:
-        case Add:
+        case "Add":
             new_todo = values["todo"] + "\n"
             todos=function.file()
             todos.append(new_todo)
+            function.write(todos)
+            window["main"].update(values=todos)
+        case "edit":
+            edit_todo=values["main"][0]
+            new_todo=values["todo"] + "\n"
+            todos = function.file()
+            index=todos.index(edit_todo)
+            todos[index]=new_todo
             function.write(todos)
             window["main"].update(values=todos)
 window.close()
