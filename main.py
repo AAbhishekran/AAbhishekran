d4f9f8db38610f6dd7b57f8f778db5d1fdddf83c
Mind game <abhirana2865@gmail.com>
2024-02-07 22:55:42 +0530

    i created the Add function in simplegui app and list function

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import PySimpleGUI as ss
+from jinja2.nodes import Add
 
+import function
+list_box = ss.Listbox(values = function.file(), key="main",size=[45,15])
 Layout = [[ss.Text("This is my todoaap",key="text")],
-          [ss.InputText(tooltip="Enter your todo id her"),ss.Button("Enter")],
-
+          [ss.InputText(tooltip="Enter your todo id her",key="todo"),
+           ss.Button("Add")],[list_box]
           ]
-window = ss.Window(title="Todosaap" , layout=Layout,font=("Bold",20))
+window = ss.Window(title="Todosaap" , layout=Layout,font=("Bold",10))
 
-
-window.read()
+while True:
+    event , values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case Add:
+            new_todo = values["todo"] + "\n"
+            todos=function.file()
+            todos.append(new_todo)
+            function.write(todos)
+            window["main"].update(values=todos)
 window.close()
